ae8dbecc7fa8033f447c48415c40bcf96e5b2b43
Dhruv Mittal <mittal2006dhruv@gmail.com>
2025-03-29 00:32:57 +0530

    Initial commit

---
+++ 01_basics/01_data-types_summary.js
@@ -0,0 +1,20 @@
+// Primitive - Stored by value and are immutable.
+
+// 7 types - String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const marks = 80 
+const percentage = 95.6
+
+const is_passed = true
+
+const outside_Temp = null
+let user_name ;
+
+const id = Symbol('325') ;
+const anotherId = Symbol('235') ;
+
+console.log(id===anotherId);    // false
+
+// Non Primitive - Stored by reference & are mutable.
+
+// Arrays, Objects, Functions
