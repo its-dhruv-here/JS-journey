ed745441eecbc90e6af17b8567fddc3e8ed1bca1
Dhruv Mittal <mittal2006dhruv@gmail.com>
2025-03-29 01:47:48 +0530

    exp. stack & heap concept

--- 01_basics/01_data-types_summary.js
+++ 01_basics/01_data-types_summary.js
@@ -35,3 +35,47 @@ my_func() ;             // calling of function
 
 
 console.log(typeof anotherId);   // we cann use typeof to get to know about the type.
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++ //
+
+
+// Primitive -> Stack   &  Non primitive -> Heap
+
+let my_name = "Dhruv" ;
+let another_name = my_name ;
+
+another_name = "Aditya" ;                       
+ /*
+ Explanation:
+1.	my_name stores the value “Dhruv”.
+2.	another_name = my_name; → This copies the value of my_name into another_name.
+->	Now, another_name is a separate copy of "Dhruv", not linked to my_name.
+3.	another_name = "Aditya"; → We change another_name to "Aditya", but my_name remains “Dhruv”` because primitive values (like strings) are stored separately. in stack
+*/
+
+console.log(my_name);           // Output: Dhruv
+console.log(another_name);      // Output: Aditya
+
+
+let userOne_details = {
+    name : "Raghu" ,
+    email_id : "xyz@google.com",
+
+}
+let usertwo_details = userOne_details ;
+
+usertwo_details.name = "Naman" ;
+/*
+Explanation:
+	1.	userOne_details is an object, storing name and email_id.
+	2.	usertwo_details = userOne_details;
+	->	This does not create a new copy!
+	->	Instead, usertwo_details now refers to the same object in memory as userOne_details.
+	3.	usertwo_details.name = "Naman";
+	->	Since both variables refer to the same object, changing usertwo_details.name also affects userOne_details.name.
+*/
+
+console.log(userOne_details);       // Output: { name: "Naman", email_id: "xyz@google.com" }
+console.log(usertwo_details);       // Output: { name: "Naman", email_id: "xyz@google.com" }
+
